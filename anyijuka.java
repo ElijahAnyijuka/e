284bea9164c3ac22e401731b46126cec6b0a679c
ElijahAnyijuka <117357473+ElijahAnyijuka@users.noreply.github.com>
2022-11-16 20:39:17 +0300

    Add files via upload

---
+++ anyijuka.java
@@ -0,0 +1,28 @@
+package elijahf;
+
+import java.util.Scanner;
+
+class year {
+	private int Age;
+	public int getAge() {
+		return Age;
+	}
+	
+public void setAge(int Age) {
+	this.Age = Age;
+}
+}
+class anyijuka extends year  {
+public static void main(string[] args) {
+		year p1 = new year ();
+		p1.setAge(24);
+		System.out.println("Year Of Entry" + p1.getAge());
+		Scanner input = new Scanner(System.in);
+		System.out.print("Enter text");
+		String myString = input.next();
+		System.out.println("Text entered " . myString);
+				
+		
+	}
+	
+}
